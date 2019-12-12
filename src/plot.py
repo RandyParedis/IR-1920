92a7ec03b12178399a7f6fa9fb1239da51f172a9
Unknown <randy.paredis@student.uantwerpen.be>
2019-12-12 12:31:38 +0100

    Made plottable for self-scoring

--- src/plot.py
+++ src/plot.py
@@ -13,6 +13,12 @@ import json
 
 class Plot:
     @staticmethod
+    def plot(ax, x, y, title='', **kwargs):
+        if len(title) > 0:
+            ax.set_title(title)
+        ax.plot(range(len(x)), y, **kwargs)
+
+    @staticmethod
     def bar(ax, x, y, title='', **kwargs):
         if len(title) > 0:
             ax.set_title(title)
@@ -48,50 +54,79 @@ def normalize(vector: list):
 
 if __name__ == '__main__':
     args = sys.argv
-    if len(args) == 2 and os.path.isfile(args[1]):
-        with open(args[1]) as json_file:
-            data = json.load(json_file)
+    if len(args) == 2 and os.path.isdir(args[1]):
+        data = {
+            "scores": {},
+            "idxs": [],
+            "time": []
+        }
+        dname = args[1]
+        files = os.listdir(dname)
+        tabs = '    ' * 13
+        for i in range(len(files)):
+            fname = files[i]
+            print("\r\tFILE %i / %i [%.2f %%] {%s}%s" % (i, len(files), i / len(files) * 100, fname, tabs), end='')
+            if fname.endswith('.json'):
+                with open(dname + fname) as json_file:
+                    d = json.load(json_file)
+                    for sc in d['scores']:
+                        if sc in data["scores"]:
+                            raise 4
+                        data["scores"][sc] = d['scores'][sc]
+                    data["idxs"] += d["idxs"]
+                    data["time"] += d["time"]
+        print("\r\tFILE %i / %i [100 %%]%s" % (len(files), len(files), tabs))
+        print(len(data["idxs"]), "records in dataset")
 
         # Plot time
-        fig, (ax1, ax2) = plt.subplots(1, 2, sharey=True, gridspec_kw={'wspace': 0, 'width_ratios': [5, 1]})
-        ax1.set_xmargin(0.01)
-        ax2.set_xmargin(0.01)
-        ax1.set_ylabel('seconds')
-        for t in ax1.get_xticklabels():
-            t.set_rotation(90)
-        fig.suptitle("Lucene Self-Scoring Duration")
-        Plot.bar(ax1, ["Q%s" % x for x in data['idxs']], data['time'])
-        Plot.boxplot(ax2, data['time'])
+        #fig, (ax1, ax2) = plt.subplots(1, 2, sharey=True, gridspec_kw={'wspace': 0, 'width_ratios': [5, 1]})
+        #ax1.set_xmargin(0.01)
+        #ax2.set_xmargin(0.01)
+        #ax1.set_ylabel('seconds')
+        #for t in ax1.get_xticklabels():
+        #    t.set_rotation(90)
+        #fig.suptitle("Lucene Self-Scoring Duration")
+        #Plot.bar(ax1, ["Q%s" % x for x in data['idxs']], data['time'])
+        #Plot.boxplot(ax2, data['time'])
+        #plt.show()
+
+        L = data["scores"]
+        fig, ax = plt.subplots()
+        ax.set_xlabel("Question ID")
+        ax.set_ylabel("Score")
+        Plot.bar(ax, ['' for x in L if x in L[x]], [L[x][x] for x in L if x in L[x]],
+                 "Self-Scoring Performance")
         plt.show()
+
 
         # Plot scores
-        N = len(data['idxs'])
-        matrix = [[0 for r in range(N)] for c in range(N)]
-        for x in data['scores']:
-            for y in data['scores'][x]:
-                matrix[int(x)][int(y)] = data['scores'][x][y]
-
-        # Compute PR-values
-        alpha = 0.5
-        TP = FP = TN = FN = 0
-        for r in range(N):
-            matrix[r] = normalize(matrix[r])
-            l = [1 if x > alpha else 0 for x in matrix[r]]
-            TP += l[r]
-            TN += sum([(x + 1) % 2 for x in l])
-            FN += 1 if l[r] == 0 else 0
-            l[r] = 0
-            FP += sum(l)
-
-        print("TP:", TP)
-        print("FP:", FP)
-        print("TN:", TN)
-        print("FN:", FN)
-        print("Precision:", TP / (TP + FP))
-        print("Recall:   ", TP / (TP + FN))
-        print("Accuracy: ", (TP + TN) / (N * N))
-
-        # Plot CM
-        fig, ax = plt.subplots()
-        Plot.confusion_matrix(ax, matrix, data['idxs'], 'Confusion Matrix')
-        plt.show()
+        # N = len(data['idxs'])
+        # matrix = [[0 for r in range(N)] for c in range(N)]
+        # for x in data['scores']:
+        #     for y in data['scores'][x]:
+        #         matrix[int(x)][int(y)] = data['scores'][x][y]
+        #
+        # # Compute PR-values
+        # alpha = 0.5
+        # TP = FP = TN = FN = 0
+        # for r in range(N):
+        #     matrix[r] = normalize(matrix[r])
+        #     l = [1 if x > alpha else 0 for x in matrix[r]]
+        #     TP += l[r]
+        #     TN += sum([(x + 1) % 2 for x in l])
+        #     FN += 1 if l[r] == 0 else 0
+        #     l[r] = 0
+        #     FP += sum(l)
+        #
+        # print("TP:", TP)
+        # print("FP:", FP)
+        # print("TN:", TN)
+        # print("FN:", FN)
+        # print("Precision:", TP / (TP + FP))
+        # print("Recall:   ", TP / (TP + FN))
+        # print("Accuracy: ", (TP + TN) / (N * N))
+        #
+        # # Plot CM
+        # fig, ax = plt.subplots()
+        # Plot.confusion_matrix(ax, matrix, data['idxs'], 'Confusion Matrix')
+        # plt.show()

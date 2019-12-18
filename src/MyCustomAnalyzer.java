7034b34c23127ca5eeaa1cac8167f41f15f548c9
joostatsooj <joostatsooj@gmail.com>
2019-12-18 22:58:11 +0100

    custom analyzer 2.0

--- src/MyCustomAnalyzer.java
+++ src/MyCustomAnalyzer.java
@@ -5,6 +5,10 @@ import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.core.LowerCaseFilter;
 import org.apache.lucene.analysis.StopFilter;
 import org.apache.lucene.analysis.en.PorterStemFilter;
+import org.apache.lucene.analysis.ngram.NGramTokenFilter;
+
+import org.apache.lucene.analysis.miscellaneous.WordDelimiterGraphFilter;
+import static org.apache.lucene.analysis.miscellaneous.WordDelimiterGraphFilter.CATENATE_ALL;
 
 // https://www.baeldung.com/lucene-analyzers
 public class MyCustomAnalyzer extends Analyzer {
@@ -13,7 +17,9 @@ public class MyCustomAnalyzer extends Analyzer {
         StandardTokenizer src = new StandardTokenizer();
         TokenStream result = new LowerCaseFilter(src);
         result = new StopFilter(result, EnglishAnalyzer.ENGLISH_STOP_WORDS_SET);
+        result = new WordDelimiterGraphFilter(result, CATENATE_ALL, null);
         result = new PorterStemFilter(result);
+        result = new NGramTokenFilter(result, 4);
         return new TokenStreamComponents(src, result);
 
     }

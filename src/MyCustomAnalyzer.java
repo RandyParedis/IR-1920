b0aa8fb0ef494068d7614811c27217c14c6e52af
joostatsooj <joostatsooj@gmail.com>
2019-12-18 21:21:06 +0100

    better

--- src/MyCustomAnalyzer.java
+++ src/MyCustomAnalyzer.java
@@ -1,6 +1,5 @@
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardTokenizer;
-import org.apache.lucene.analysis.standard.StandardFilter;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.core.LowerCaseFilter;
@@ -8,15 +7,15 @@ import org.apache.lucene.analysis.StopFilter;
 import org.apache.lucene.analysis.en.PorterStemFilter;
 import org.apache.lucene.analysis.Analyzer.TokenStreamComponents;
 import org.apache.lucene.analysis.*;
+import org.apache.lucene.analysis.core.StopAnalyzer;
 
 // https://www.baeldung.com/lucene-analyzers
 public class MyCustomAnalyzer extends Analyzer {
     @Override
     protected TokenStreamComponents createComponents(String fieldName) {
         StandardTokenizer src = new StandardTokenizer();
-        TokenStream result = new StandardFilter(src);
-        result = new LowerCaseFilter(result);
-        result = new StopFilter(result,  StandardAnalyzer.ENGLISH_STOP_WORDS_SET);
+        TokenStream result = new LowerCaseFilter(src);
+//        result = new StopFilter(result, StopAnalyzer.ENGLISH_STOP_WORDS_SET);
         result = new PorterStemFilter(result);
         return new TokenStreamComponents(src, result);
 

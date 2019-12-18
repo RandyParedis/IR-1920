531859079c2576e1d58b1439d362d61ddd8e3ffa
Unknown <randy.paredis@student.uantwerpen.be>
2019-12-18 21:31:01 +0100

    Try this?

--- src/MyCustomAnalyzer.java
+++ src/MyCustomAnalyzer.java
@@ -1,13 +1,10 @@
 import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.en.EnglishAnalyzer;
 import org.apache.lucene.analysis.standard.StandardTokenizer;
-import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.core.LowerCaseFilter;
 import org.apache.lucene.analysis.StopFilter;
 import org.apache.lucene.analysis.en.PorterStemFilter;
-import org.apache.lucene.analysis.Analyzer.TokenStreamComponents;
-import org.apache.lucene.analysis.*;
-import org.apache.lucene.analysis.core.StopAnalyzer;
 
 // https://www.baeldung.com/lucene-analyzers
 public class MyCustomAnalyzer extends Analyzer {
@@ -15,7 +12,7 @@ public class MyCustomAnalyzer extends Analyzer {
     protected TokenStreamComponents createComponents(String fieldName) {
         StandardTokenizer src = new StandardTokenizer();
         TokenStream result = new LowerCaseFilter(src);
-//        result = new StopFilter(result, StopAnalyzer.ENGLISH_STOP_WORDS_SET);
+        result = new StopFilter(result, EnglishAnalyzer.ENGLISH_STOP_WORDS_SET);
         result = new PorterStemFilter(result);
         return new TokenStreamComponents(src, result);
 

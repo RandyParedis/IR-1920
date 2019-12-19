ed3fa797a8f45cde877ff428f0e9b6b1cefe11bb
joostatsooj <joostatsooj@gmail.com>
2019-12-19 19:05:57 +0100

    added synonyms

--- run-joost.sh
+++ run-joost.sh
@@ -2,7 +2,7 @@
 
 bash ./clear.sh
 echo "Compiling..."
-export CLASSPATH=$CLASSPATH:$(find ~/Documents/Overig/lucene-8.3.0/ -name "lucene*.jar" | paste -sd ":" -)
+export CLASSPATH=$CLASSPATH:$(find ~/Documents/Overig/lucene-8.3.0/ -name "lucene*.jar" | paste -sd ":" -):~/Documents/Overig/json-simple-1.1.1.jar
 javac src/*.java
 echo "Compiled!"
 mv $(find src -name "*.class") .
--- src/MyCustomAnalyzer.java
+++ src/MyCustomAnalyzer.java
@@ -6,12 +6,48 @@ import org.apache.lucene.analysis.core.LowerCaseFilter;
 import org.apache.lucene.analysis.StopFilter;
 import org.apache.lucene.analysis.en.PorterStemFilter;
 import org.apache.lucene.analysis.ngram.NGramTokenFilter;
+import org.apache.lucene.analysis.synonym.SynonymFilter;
 
 import org.apache.lucene.analysis.miscellaneous.WordDelimiterGraphFilter;
 import static org.apache.lucene.analysis.miscellaneous.WordDelimiterGraphFilter.CATENATE_ALL;
 
+import java.io.*;
+import java.nio.file.Paths;
+import java.util.*;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.apache.lucene.analysis.synonym.SynonymMap;
+import org.apache.lucene.util.CharsRef;
+
 // https://www.baeldung.com/lucene-analyzers
 public class MyCustomAnalyzer extends Analyzer {
+    private SynonymMap generateSynonymMap() {
+        BufferedReader reader;
+        try {
+            SynonymMap.Builder builder = new SynonymMap.Builder(true);
+            reader = new BufferedReader(new FileReader("data/en_thesaurus_synonyms.txt"));
+            String line = reader.readLine();
+            while (line != null) {
+                JSONParser parser = new JSONParser();
+                JSONObject json = (JSONObject) parser.parse(line);
+                String word = (String) json.get("word");
+                JSONArray jsonArray = (JSONArray) json.get("synonyms");
+                jsonArray.forEach(item -> {
+//                    System.out.println(word + ": " + item);
+                    builder.add(new CharsRef(word), new CharsRef((String) item), true);
+                });
+                line = reader.readLine();
+            }
+            reader.close();
+            SynonymMap synMap = builder.build();
+            return synMap;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
     @Override
     protected TokenStreamComponents createComponents(String fieldName) {
         StandardTokenizer src = new StandardTokenizer();
@@ -19,6 +55,7 @@ public class MyCustomAnalyzer extends Analyzer {
         result = new StopFilter(result, EnglishAnalyzer.ENGLISH_STOP_WORDS_SET);
         result = new WordDelimiterGraphFilter(result, CATENATE_ALL, null);
         result = new PorterStemFilter(result);
+        result = new SynonymFilter(result, generateSynonymMap(), true);
         result = new NGramTokenFilter(result, 4);
         return new TokenStreamComponents(src, result);
 

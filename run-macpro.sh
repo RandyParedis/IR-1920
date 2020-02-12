00b15ebe9c0a672428deaacd25e51ca8c2744062
joostatsooj <joostatsooj@gmail.com>
2020-02-12 21:28:10 +0100

    run script macpro 2.0

--- run-macpro.sh
+++ run-macpro.sh
@@ -2,9 +2,9 @@
 
 bash ./clear.sh
 echo "Compiling..."
-export CLASSPATH=$CLASSPATH:$(find ~/Documents/lucene-8.3.0/ -name "lucene*.jar" | paste -sd ":" -):./packages/json-simple-1.1.1.jar
+export CLASSPATH=$CLASSPATH:$(find ~/Documents/Software/lucene-8.3.0/ -name "lucene*.jar" | paste -sd ":" -):./packages/json-simple-1.1.1.jar
 javac src/*.java
 echo "Compiled!"
 mv $(find src -name "*.class") .
-java Main /media/joost/Data_0/python_cpp
+java Main /media/joost/Data0/python_cpp
 #java Main smallPosts

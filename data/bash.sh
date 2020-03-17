a614d9e44b0a2bcc8068376f7efe6b68c70d5cb0
joostatsooj <joostatsooj@gmail.com>
2020-03-17 16:45:45 +0100

    small fix bash

--- data/bash.sh
+++ data/bash.sh
@@ -6,6 +6,7 @@ while IFS= read -r line
 do
   for i in $(echo $line | sed "s/,/ /g")
   do
-    cp `find /media/joost/Data0/python_cpp -name "question$i.txt"` ~/Documents/UA/IR-1920/smallPosts
+    echo "Collect question $i"
+    cp `find /media/joost/Data0/python_cpp -name "question$i.xml"` /home/joost/Documents/UA/IR-1920/smallPosts
   done
 done < "$input"

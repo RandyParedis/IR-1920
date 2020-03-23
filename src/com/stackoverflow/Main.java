a10b7fe452c6ffe1d2ddd4c9b15bffad121e211c
Unknown <randy.paredis@student.uantwerpen.be>
2020-03-23 21:49:26 +0100

    Added simple querey checker

--- src/com/stackoverflow/Main.java
+++ src/com/stackoverflow/Main.java
@@ -10,6 +10,7 @@ import com.stackoverflow.searching.SearchEngine;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -25,39 +26,52 @@ public class Main {
 
         // Load all queries
         QueryLoader queryLoader = new QueryLoader(engine.getDirectory(), engine.getReader(), analyzer);
-        List<Query> queries = queryLoader.getQueries("suggestionsQuery.txt", SUGGESTIONS,
-                (String q) -> q.substring(3));
 
-        // Write all matches to file in the same format as the manual labeling happened
-        // TODO: do things with matched documents
-        List<String> labels = new ArrayList<>();
-        for(int qid = 0; qid < queries.size(); ++qid) {
-            // Find the matching documents
-            Query query = queries.get(qid);
-            Map<Double, Document> documents = engine.search(query);
-
-
-            // Store for file writing
-            List<String> ids = new ArrayList<>();
-            for(Map.Entry<Double, Document> pair: documents.entrySet()) {
-                ids.add(SearchEngine.questionID(pair.getValue().get("name")));
+        // Adapt the arguments of the function to your likings :D
+        List<String> qsts = Arrays.asList("Django", "Django Development", "My very fancy query");
+        for(String qst: qsts) {
+            Query q = queryLoader.getQuery(qst, SUGGESTIONS);
+            Map<Double, Document> documents = engine.search(q);
+            System.out.println("\n" + documents.size() + " result(s) for '" + qst + "':");
+            for(Map.Entry<Double, Document> en: documents.entrySet()) {
+                System.out.println("\t" + en.getKey() + ": " + en.getValue().get("name"));
             }
-            String results =  String.format("%02d", qid);
-
-            // Special ids:
-            if(qid == queries.size() - 2) { // 98 is Python
-                results = "98";
-            } else if(qid == queries.size() - 1) { // 99 is CPP
-                results = "99";
-            }
-            results += ":" + String.join(",", ids);
-            labels.add(results);
+            System.out.println();
         }
-        BufferedWriter writer = new BufferedWriter(new FileWriter("data/labels.txt"));
-        writer.write(String.join("\n", labels));
-        writer.close();
 
-        // TODO: Mark as relevant and generate plot data
-        RelevanceMarker relevanceMarker = new RelevanceMarker();
+//        List<Query> queries = queryLoader.getQueries("suggestionsQuery.txt", SUGGESTIONS,
+//                (String q) -> q.substring(3));
+//
+//        // Write all matches to file in the same format as the manual labeling happened
+//        // TODO: do things with matched documents
+//        List<String> labels = new ArrayList<>();
+//        for(int qid = 0; qid < queries.size(); ++qid) {
+//            // Find the matching documents
+//            Query query = queries.get(qid);
+//            Map<Double, Document> documents = engine.search(query);
+//
+//
+//            // Store for file writing
+//            List<String> ids = new ArrayList<>();
+//            for(Map.Entry<Double, Document> pair: documents.entrySet()) {
+//                ids.add(SearchEngine.questionID(pair.getValue().get("name")));
+//            }
+//            String results =  String.format("%02d", qid);
+//
+//            // Special ids:
+//            if(qid == queries.size() - 2) { // 98 is Python
+//                results = "98";
+//            } else if(qid == queries.size() - 1) { // 99 is CPP
+//                results = "99";
+//            }
+//            results += ":" + String.join(",", ids);
+//            labels.add(results);
+//        }
+//        BufferedWriter writer = new BufferedWriter(new FileWriter("data/labels.txt"));
+//        writer.write(String.join("\n", labels));
+//        writer.close();
+//
+//        // TODO: Mark as relevant and generate plot data
+//        RelevanceMarker relevanceMarker = new RelevanceMarker();
     }
 }
